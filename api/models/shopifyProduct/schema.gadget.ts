59771c964b4ace859a4569559ff10dfea9ffaf4a
Jonathan Walker <jwalker@refinerfireweb.com>
2024-12-14 14:14:44 -0500

    feat: Create product model

---
+++ api/models/shopifyProduct/schema.gadget.ts
@@ -0,0 +1,31 @@
+import type { GadgetModel } from "gadget-server";
+
+// This file describes the schema for the "shopifyProduct" model, go to https://bladeshark.gadget.app/edit to view/edit your model in Gadget
+// For more information on how to update this file http://docs.gadget.dev
+
+export const schema: GadgetModel = {
+  type: "gadget/model-schema/v1",
+  storageKey: "DataModel-Shopify-Product",
+  fields: {},
+  shopify: {
+    fields: [
+      "body",
+      "category",
+      "compareAtPriceRange",
+      "customCollections",
+      "handle",
+      "hasVariantsThatRequiresComponents",
+      "orderLineItems",
+      "productType",
+      "publishedAt",
+      "shop",
+      "shopifyCreatedAt",
+      "shopifyUpdatedAt",
+      "status",
+      "tags",
+      "templateSuffix",
+      "title",
+      "vendor",
+    ],
+  },
+};
